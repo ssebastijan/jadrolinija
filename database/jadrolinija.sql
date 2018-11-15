e059407c9740cbdd1a6919c88d30b89931eb3c56
Sebastijan Sakac <sebastijan.sakac@icloud.com>
2018-11-15 07:12:56 +0100

    dodano pristaniste u bazu podataka

--- database/jadrolinija.sql
+++ database/jadrolinija.sql
@@ -31,6 +31,23 @@ CREATE TABLE `luka` (
 ) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
+--
+-- Table structure for table `pristaniste`
+--
+
+DROP TABLE IF EXISTS `pristaniste`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+ SET character_set_client = utf8mb4 ;
+CREATE TABLE `pristaniste` (
+  `sifPristanista` int(11) NOT NULL AUTO_INCREMENT,
+  `sifraLuke` int(11) NOT NULL,
+  `kapacitetBroda` int(11) NOT NULL,
+  `sifraNadredenogPristanista` int(11) DEFAULT NULL,
+  PRIMARY KEY (`sifPristanista`),
+  KEY `sifraLuke` (`sifraLuke`),
+  CONSTRAINT `pristaniste_ibfk_1` FOREIGN KEY (`sifraLuke`) REFERENCES `luka` (`sifraluke`) ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -41,4 +58,4 @@ CREATE TABLE `luka` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-11-15  7:07:50
+-- Dump completed on 2018-11-15  7:11:14
