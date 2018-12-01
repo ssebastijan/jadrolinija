57aebe0808a3d6cb83d4ce758695afaee174bd63
Sebastijan Sakac <sebastijan.sakac@icloud.com>
2018-12-01 10:55:30 +0100

    dodana zabrana smanjenja broja pristanista u luci, ako vec postoji vise pristanista, u bazi podataka

--- database/jadrolinija.sql
+++ database/jadrolinija.sql
@@ -27,7 +27,7 @@ CREATE TABLE `brod` (
   `nazivBrod` varchar(50) NOT NULL,
   `kapacitetPutnici` int(11) NOT NULL,
   PRIMARY KEY (`sifBrod`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -51,7 +51,7 @@ CREATE TABLE `karta` (
   CONSTRAINT `karta_ibfk_1` FOREIGN KEY (`sifPutnika`) REFERENCES `putnik` (`sifputnik`) ON UPDATE CASCADE,
   CONSTRAINT `karta_ibfk_2` FOREIGN KEY (`sifLinije`) REFERENCES `linija` (`sifralinije`) ON UPDATE CASCADE,
   CONSTRAINT `karta_ibfk_3` FOREIGN KEY (`sifBroda`) REFERENCES `brod` (`sifbrod`) ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -82,10 +82,10 @@ CREATE TABLE `linija` (
 DELIMITER ;;
 /*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER `provjeriLiniju` BEFORE INSERT ON `linija` FOR EACH ROW BEGIN
         IF TIME_TO_SEC(NEW.satDolaska) <= TIME_TO_SEC(NEW.satOdlaska) THEN
-        	signal sqlstate '45001' set message_text = 'Vrijeme dolaska mora biti vece od vremena odlaska', MYSQL_ERRNO = 1001;
+        	signal sqlstate '45001' set message_text = 'Vrijeme dolaska mora biti veće od vremena odlaska', MYSQL_ERRNO = 1001;
         END IF;
         IF NEW.sifraOdlaznogPristanista = NEW.sifraDolaznogPristanista THEN
-        	signal sqlstate '45002' set message_text = 'Odlazno i dolazno pristaniste moraju biti razliciti', MYSQL_ERRNO = 1002;
+        	signal sqlstate '45002' set message_text = 'Odlazno i dolazno pristanište moraju biti različiti', MYSQL_ERRNO = 1002;
         END IF;
 	END */;;
 DELIMITER ;
@@ -107,8 +107,29 @@ CREATE TABLE `luka` (
   `nazivMjesta` varchar(255) NOT NULL,
   `brojPristanista` int(11) NOT NULL,
   PRIMARY KEY (`sifraLuke`)
-) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'NO_AUTO_VALUE_ON_ZERO' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER `updateLuka` BEFORE UPDATE ON `luka` FOR EACH ROW BEGIN
+        DECLARE trenutnoPristanista INT DEFAULT 0;
+        SELECT count(*) INTO trenutnoPristanista from pristaniste WHERE sifraLuke = new.sifraLuke;
+        IF NEW.brojPristanista < trenutnoPristanista THEN 
+          signal sqlstate '45003' SET message_text = 'Ne možete smanjiti broj pristaništa', MYSQL_ERRNO = 1003;
+        END IF;
+  END */;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
 
 --
 -- Table structure for table `pristaniste`
@@ -126,7 +147,7 @@ CREATE TABLE `pristaniste` (
   PRIMARY KEY (`sifPristanista`),
   KEY `sifraLuke` (`sifraLuke`),
   CONSTRAINT `pristaniste_ibfk_1` FOREIGN KEY (`sifraLuke`) REFERENCES `luka` (`sifraluke`) ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!50003 SET @saved_cs_client      = @@character_set_client */ ;
 /*!50003 SET @saved_cs_results     = @@character_set_results */ ;
@@ -166,7 +187,7 @@ CREATE TABLE `putnik` (
   `brojPutovnice` varchar(50) NOT NULL,
   `drzavljanstvo` varchar(50) NOT NULL,
   PRIMARY KEY (`sifPutnik`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=0 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -178,4 +199,4 @@ CREATE TABLE `putnik` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-11-15  7:11:14
+-- Dump completed on 2018-12-01 10:52:50
