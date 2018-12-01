037408b309a62cfbd55088ccc8549e52d18ba024
Sebastijan Sakac <sebastijan.sakac@icloud.com>
2018-12-01 12:27:20 +0100

    dodan triger za update pristanista

--- database/jadrolinija.sql
+++ database/jadrolinija.sql
@@ -172,6 +172,25 @@ DELIMITER ;
 /*!50003 SET character_set_client  = @saved_cs_client */ ;
 /*!50003 SET character_set_results = @saved_cs_results */ ;
 /*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'NO_AUTO_VALUE_ON_ZERO' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER `updatePistaniste` BEFORE UPDATE ON `pristaniste` FOR EACH ROW BEGIN
+        if new.sifPristanista = new.sifraNadredenogPristanista THEN
+          signal sqlstate '45004' set message_text = 'Nadredjeno pristanište ne smije biti isto kao i ovo pristanište', MYSQL_ERRNO = 1004;
+        END IF;
+  END */;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
 
 --
 -- Table structure for table `putnik`
